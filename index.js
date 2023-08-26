7defc550e5bc8350dbd9de465df5b4dad60b0096
Hemsas Riane <rayan.hemsas@gmail.com>
2023-08-26 17:50:15 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+
+  static capitalize (string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+
+  }
+  static sanitize (string){
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+  static titleize (string){
+   
+const otherWords=['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+const words=string.split(' ').map(word =>{
+  if(otherWords.includes(word)){
+return word;
+  }else {
+    return this.capitalize(word);
+  }
+});
+return this.capitalize(words.join(' '));
+  }
+
 }
